7452d0dfe3b000a5949909bbff82e74bfdb47849
Abdulirhman Ashraf <sabdo3649@gmail.com>
2026-07-08 18:25:27 +0300

    Implement PWA features, update navigation menus, and add new images

--- js/master.js
+++ js/master.js
@@ -1,26 +1,59 @@
 let card = document.querySelectorAll("#collection a");
 let btn = document.querySelector("#show");
+// PWA Button
+
+let installBtn = document.getElementById("install");
+let deferredPrompt;
+window.addEventListener("beforeinstallprompt", (e) => {
+  // stop there browser
+  e.preventDefault();
+  // plz wait here and dont worry
+  deferredPrompt = e;
+  installBtn.style.display = "block";
+});
+// button click function
+
+if(installBtn){
+
+  installBtn.addEventListener("click", async (e) => {
+    // 1-is hijack success? yes
+    if (deferredPrompt) {
+      // 2-show installation options
+      
+      deferredPrompt.prompt();
+      // 3-store user choice in outcome
+      const { outcome } = await deferredPrompt.userChoice;
+      // 4-if user accepted display none to button
+      
+      if (outcome === "accepted") {
+        installBtn.style.display = "none";
+      } else {
+      console.log("user dismissed");
+    }
+    deferredPrompt = null;
+  }
+});
+}
 // navbar for mobile
 let btnMenu = document.querySelector(".menu");
 
-let uls = document.querySelector("ul");
+let uls = document.querySelector(".ulMenu");
 
-btnMenu.addEventListener("click",function(){
-    uls.classList.toggle("toggleMeu")
-})
+btnMenu.addEventListener("click", function () {
+  uls.classList.toggle("toggleMenu");
+});
 // textArea Limit Character
-let max=document.querySelector(".max");
-let textarea=document.querySelector("textarea");
-function textLimit(){
-  max.innerHTML =`Remaining Character : ${250-textarea.value.length}`;
+let max = document.querySelector(".max");
+let textarea = document.querySelector("textarea");
+function textLimit() {
+  max.innerHTML = `Remaining Character : ${250 - textarea.value.length}`;
 }
 
 // show more function
 btn.addEventListener("click", function show() {
-  if ((btn.innerHTML == "Show more")) {
+  if (btn.innerHTML == "Show more") {
     btn.innerHTML = "Hide";
-  } 
-  else if ((btn.innerHTML == "Hide")) {
+  } else if (btn.innerHTML == "Hide") {
     btn.innerHTML = "Show more";
   }
 
@@ -29,3 +62,10 @@ btn.addEventListener("click", function show() {
     card[i].classList.toggle("hidden");
   }
 });
+
+if(navigator.serviceWorker){
+  navigator.serviceWorker.register("../sw.js")
+  .then((reg)=>{
+console.log("file is register" ,reg)
+  }).catch((err)=>console.log("Error",err))
+}
---
+++ sw.js
@@ -0,0 +1,69 @@
+const cacheName = "Portfolio";
+const assets = [
+  "/",
+  "/index.html",
+  "/about.html",
+  "/contact.html",
+  "/projects.html",
+  "/style/style.css",
+  "/style/all.min.css",
+  "/js/master.js",
+  "/imags/android/launchericon-48x48.png",
+  "/imags/android/launchericon-72x72.png",
+  "/imags/android/launchericon-96x96.png",
+  "/imags/android/launchericon-144x144.png",
+  "/imags/android/launchericon-192x192.png",
+  "/imags/android/launchericon-512x512.png",
+  "/imags/quiz.png",
+  "/imags/my.png",
+  "/imags/site.png",
+  "/imags/step.png",
+  "/imags/todo.png",
+  "/imags/weather.png",
+  "/manifest.json",
+  "https://unpkg.com/aos@2.3.1/dist/aos.css",
+  "https://unpkg.com/aos@2.3.1/dist/aos.js",
+];
+// store data from server to cache store
+self.addEventListener("install", (e) => {
+  // plz wait until my code end then start
+  e.waitUntil(
+    // make folder with my cache
+    caches
+      .open(cacheName)
+      // store my assets in this folder
+      .then((cache) => {
+        cache.addAll(assets);
+      })
+      .catch((err) => console.log("Error", err)),
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  // plz wait until success my promise
+  e.waitUntil(
+    // Give me all caches
+    caches.keys().then((keys) => {
+      // wait all
+      return Promise.all(
+        // where is my elders sons ?
+        keys
+          .filter((key) => key != cacheName)
+          //goodbey, i love only your youngsters
+          .map((key) => caches.delete(key)),
+      );
+    }),
+  );
+});
+
+// fetch data s.w pov
+self.addEventListener("fetch", (e) => {
+    // wait! wait! browser,i will do that 
+  e.respondWith(
+    // so let's see if i have your request
+    caches.match(e.request)
+    // yes yes here you are
+    .then((res) => res || 
+    // iam sorry let ask server
+    fetch(e.request)));
+});
